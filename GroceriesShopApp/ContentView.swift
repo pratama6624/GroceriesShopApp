c9e51b56edb5be51fa58211d80854e38acbf8fd2
pratama6624 <scyoung6624@gmail.com>
2025-01-11 22:03:58 +0700

    Initial Commit

---
+++ GroceriesShopApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceriesShopApp
+//
+//  Created by Pratama One on 11/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceriesShopApp/GroceriesShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesShopAppApp.swift
+//  GroceriesShopApp
+//
+//  Created by Pratama One on 11/01/25.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
